6f741618edea3d878fcf854b413459864a76bfa0
Alex Tarakanoff <post@linuxmasterz.ru>
2015-08-22 09:35:50 +0500

    Update fortuneteller.sh

--- fortuneteller.sh
+++ fortuneteller.sh
@@ -13,7 +13,7 @@
 # Comments:
 # You have to install FOSS three programs: fortune, pinpoint, avconv
 # sudo apt-get install fortunes pinpoint libav-tools
-# And also you have to add udev script 90-usb-fortunes.rules to /etc/udev/rules.d:
+# And also you have to add udev script 90-usb-fortuneteller.rules to /etc/udev/rules.d:
 # ACTION=="add", DEVPATH=="/devices/pci0000:00/0000:00:1a.0/usb1/1-1/1-1.2" RUN+="/bin/bash /home/omsklug/scripts/fortuneteller.sh"
 # ACTION=="remove", DEVPATH=="/devices/pci0000:00/0000:00:1a.0/usb1/1-1/1-1.2" RUN+="/usr/bin/pkill '^pinpoint*'"
 # ...where DEVPATH is your favorite USB port for connection
